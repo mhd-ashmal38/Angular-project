c6568ce27201c536acfba74437430c1460be2d50
mhd-ashmal38 <mhdashmal38@gmail.com>
2024-08-09 15:09:48 +0530

    feat: add tickets and mark tickets

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  @Input({ required: true }) data!: Ticket;
+  @Output() close = new EventEmitter;
+  detailsVisible = false;
 
+  onToggleDetails() {
+    this.detailsVisible = !this.detailsVisible;
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
